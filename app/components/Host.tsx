88238a605a9b0750e902ae06a66825de4f05e459
ssk-12 <ullegadda.srikanta@gmail.com>
2024-05-07 18:32:46 +0530

    fixed marking attendance for attendee

--- app/components/Host.tsx
+++ app/components/Host.tsx
@@ -4,23 +4,25 @@ import Stream from './Stream';
 import CreateEvent from './CreateEvent';
 import Events from './Events';
 
-function Host({class_id, events}:any) {
-    const[selected, setSelected] = useState("Create event");
+function Host({ class_id, events }: any) {
+    const [selected, setSelected] = useState("Create event");
     return (
         <div className='h-full w-full flex'>
             <div style={{ width: "25%" }} className='h-full flex flex-col justify-center items-center'>
 
-                <div className=' p-2'>
-                    <button onClick={()=>{setSelected("See events")}} className=' bg-black text-white min-w-[150px] rounded-2xl px-6 py-2' type="button">See events</button>
+                <div className='h-[289px] w-[200px] flex flex-col rounded-2xl justify-center items-center bg-slate-900'>
+                    <div className=' p-2'>
+                        <button onClick={() => { setSelected("See events") }} className=' bg-black text-white min-w-[150px] hover:bg-white hover:text-black rounded-2xl px-6 py-2' type="button">See events</button>
+                    </div>
+                    <div>
+                        <button onClick={() => { setSelected("Create event") }} className='hover:bg-white hover:text-black bg-black text-white min-w-[150px] rounded-2xl px-6 py-2' type="button">Create event</button>
+                    </div>
                 </div>
-                <div>
-                    <button onClick={()=>{setSelected("Create event")}} className=' bg-black text-white min-w-[150px] rounded-2xl px-6 py-2' type="button">Create event</button>
-                </div>
-                
+
             </div>
             <div style={{ width: "75%" }} className=' h-full flex justify-center items-center'>
-                {(selected === "Create event") && <CreateEvent classId={class_id}/>}
-                {(selected === "See events") && <Events class_id = {class_id} events={events} user={false} />}
+                {(selected === "Create event") && <CreateEvent classId={class_id} />}
+                {(selected === "See events") && <Events class_id={class_id} events={events} user={false} />}
             </div>
         </div>
     )
