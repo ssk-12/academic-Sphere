164a674a87b157c6ec11321521d330c75e94dda3
ssk-12 <ullegadda.srikanta@gmail.com>
2024-05-08 12:37:23 +0530

    redirect

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,26 +1,36 @@
 import { Suspense } from 'react';
 import { fetchClass } from "@/app/lib/actions/fetchClasses";
 import { RenderClasses } from '@/app/components/Classes';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/lib/auth';
+import { redirect } from 'next/navigation';
 
 
 
 async function Classes() {
 
-    const {classes,class_enrollments} = await fetchClass();
+    const session = await getServerSession(authOptions);
+    if (!session?.user) {
+        redirect('/api/auth/signin')
+    }
+
+    const { classes, class_enrollments } = await fetchClass();
     // console.log(classes)
-     const newclasses = [...classes, ...class_enrollments];
+    const newclasses = [...classes, ...class_enrollments];
+
+    
 
 
 
 
     return (
         <div>
-        {newclasses.length > 0 ? (
-            <RenderClasses classes={newclasses}></RenderClasses>
-        ) : (
-            <p className="font-semibold flex items-center justify-center min-h-[calc(100vh-46px)]">No Classes</p>
-        )}
-    </div>
+            {newclasses.length > 0 ? (
+                <RenderClasses classes={newclasses}></RenderClasses>
+            ) : (
+                <p className="font-semibold flex items-center justify-center min-h-[calc(100vh-46px)]">No Classes</p>
+            )}
+        </div>
     )
 }
 
